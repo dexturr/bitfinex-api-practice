cc7909cc781037f9775677340538f31e0a9452ea
Dexter Edwards <dexter.edwards93@gmail.com>
2019-08-17 14:24:43 +0100

    Test using store

--- src/App.test.js
+++ src/App.test.js
@@ -20,5 +20,31 @@ it('it renders home component once loading is complete', function() {
   const component = renderComponent(App);
   store.dispatch(createWsConnected());
   component.update();
+
+  const msg1 = {type:'WS_SUBSCRIPTION_SUCCESSFUL',payload:{event:'subscribed',channel:'ticker',chanId:1,symbol:'tBTCUSD',pair:'BTCUSD'}};
+
+  const test = {
+    type: 'WS_SUBSCRIPTION_UPDATE',
+    payload: {
+      channelId: 1, 
+      data: { 
+        bid: 10320, 
+        bidSize: 84.98028129, 
+        ask: 10321, 
+        askSize: 68.8741878, 
+        dailyChange: 126, 
+        dailyChangePerc: 0.0124, 
+        lastPrice: 10320, 
+        volume: 6357.57073259, 
+        high: 10568, 
+        low: 10052
+      }
+    }
+  };
+  store.dispatch(msg1);
+  store.dispatch(test);
+  component.update();
+  
   expect(component.find('Home')).to.have.lengthOf(1);
+  console.log(component.html())
 });
