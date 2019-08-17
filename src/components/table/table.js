abd70677c11228c08e657460ffc8e37c4d47a2a1
Dexter Edwards <dexter.edwards93@gmail.com>
2019-08-18 00:21:15 +0100

    Ticket Table

---
+++ src/components/table/table.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default ({ rows, headers }) => {
+    return (
+        <table className="table">
+        <thead>
+          <tr>
+            {
+                Object.keys(headers).map(
+                (key) => <th scope="col">{key}</th>)
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => (
+            <tr key={row.symbol}>
+                {
+                    Object
+                        .values(headers)
+                        .map(
+                            (property) => <td key={property}>{row[property]}</td>
+                        )
+                }
+            </tr>
+          ))} 
+        </tbody>
+      </table>
+    )
+}
--- src/components/ticker-table/ticker-table.js
+++ src/components/ticker-table/ticker-table.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import camelcase from 'camelcase';
+import Table from '../table/table';
 
 const tradingPairSchema =  [
     'SYMBOL',
@@ -47,6 +48,13 @@ const tradingPairSchemaMapper = schemaToObjectMaker(tradingPairSchema);
 
 export default (props) => {
     const [ loading, setLoading ] = useState(true);
+    const [ rows, setRows ] = useState([]);
+    const headers = {
+        'Name': 'symbol',
+        'Last': 'lastPrice',
+        '24hr': 'dailyChange',
+        'Volume': 'volume',
+    };
     useEffect(() => {
         const fetchTickers = async () => {
             const result = await fetch('https://api-pub.bitfinex.com/v2/tickers?symbols=ALL');
@@ -58,41 +66,10 @@ export default (props) => {
                     : tradingPairSchemaMapper;
                 return mapper(tickerData);
             });
-            console.log(tickers);
+            setRows(tickers);
             setLoading(false);
         }
         fetchTickers();
-    });
-    return loading ? 'Loading' : (
-        <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Last</th>
-            <th scope="col">24hr</th>
-            <th scope="col">Vol</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-    )
+    }, []);
+    return loading ? 'Loading' : <Table rows={rows} headers={headers} />
 }
