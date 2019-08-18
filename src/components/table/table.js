dd4abce3e8c8fe6edf69a5515966f23b06c299c4
Dexter Edwards <dexter.edwards93@gmail.com>
2019-08-18 21:58:39 +0100

    Table component with memoized sort function

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Table, Column } from 'react-virtualized'
+import _ from 'lodash';
 
 const VirtualizedTable = ({ rows, headers }) => {
+  const [sortedRows, setSortedRows] = useState(rows);
+  const keys = Object.keys(headers);
+  const [defaultSortKey] = Object.values(headers);
+  const [sortDirection, setSortDirection] = useState('ASC');
+  const [sortKey, setSortKey] = useState(defaultSortKey);
+
+  const performSort = ({sortBy, sortDirection}) => {
+    setSortDirection(sortDirection);
+    setSortKey(sortBy);
+    sort();
+  };
+
+  const sort = useCallback(() => {
+    const newlySortedRows = _.orderBy(rows, [sortKey], [sortDirection.toLowerCase()]);
+    setSortedRows(newlySortedRows);
+  }, [rows, sortKey, sortDirection]);
+
   return (
     <Table
       width={800}
@@ -10,9 +28,12 @@ const VirtualizedTable = ({ rows, headers }) => {
       headerHeight={50}
       rowHeight={30}
       rowCount={rows.length}
-      rowGetter={({ index }) => rows[index]}
+      rowGetter={({ index }) => sortedRows[index]}
+      sort={performSort}
+      sortBy={sortKey}
+      sortDirection={sortDirection}
     >
-    {Object.keys(headers).map(key =>
+    {keys.map(key =>
       (<Column
         key={key}
         label={key}
