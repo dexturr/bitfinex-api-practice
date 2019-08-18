64f53f2c04b7efc6a57a0748ae72518b58df8c5c
Dexter Edwards <dexter.edwards93@gmail.com>
2019-08-18 20:12:19 +0100

    Add explanation of weird half baked code

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,23 @@ import { expect } from 'chai';
 import App from './App';
 
 // TODO: I need to mock the websocket connect in order to mock server responses for acceptance testing
+// using jest-websocket-mock I get test timeouts due to async functions still execting.
+// need some kind of server.send(/** message detail */) function that correctly mocks WS responses.
+
+// Then I can have full acceptance tests:
+/**
+ * it('should update state when receiving server message', function() {
+ *  const component = setupTest(props);
+ *  server.send(msg);
+ *  component.update();
+ *  server.messages.includes(subscriptionMessage);
+ *  server.send(msg);
+ *  component.update();
+ *  expect(component).to.have.updated.some.property;
+ *  component.detach();
+ *  server.messages.includes(unsubscribeMessage);
+ * })
+ */
 
 // import { createWsConnected } from './redux/action-creators';
 
@@ -19,36 +36,3 @@ it('it renders a loading state initially', function() {
   const component = renderComponent(App)
   expect(component.find('div').text()).to.equal('Loading');
 });
-
-// it('it renders home component once loading is complete', function() {
-//   const component = renderComponent(App);
-//   store.dispatch(createWsConnected());
-//   component.update();
-
-//   const msg1 = {type:'WS_SUBSCRIPTION_SUCCESSFUL',payload:{event:'subscribed',channel:'ticker',chanId:1,symbol:'tBTCUSD',pair:'BTCUSD'}};
-
-//   const test = {
-//     type: 'WS_SUBSCRIPTION_UPDATE',
-//     payload: {
-//       channelId: 1, 
-//       data: { 
-//         bid: 10320, 
-//         bidSize: 84.98028129, 
-//         ask: 10321, 
-//         askSize: 68.8741878, 
-//         dailyChange: 126, 
-//         dailyChangePerc: 0.0124, 
-//         lastPrice: 10320, 
-//         volume: 6357.57073259, 
-//         high: 10568, 
-//         low: 10052
-//       }
-//     }
-//   };
-//   store.dispatch(msg1);
-//   store.dispatch(test);
-//   component.update();
-  
-//   expect(component.find('Home')).to.have.lengthOf(1);
-//   console.log(component.html())
-// });
